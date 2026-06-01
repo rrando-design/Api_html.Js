36dc55fbf58280deb46a8cc55998206847cc8abd
rrando-design <rrando@obralapiedad.com.ar>
2026-06-01 11:23:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const boton= document.getElementById("buscar");
+const pokemonDiv= document.getElementById("pokemon");
+
+
+boton.addEventListener("click", async()=>{
+    //esto llama al PonerPokemon, obtiene lo que tiene la caja de texto, y el lower lo convierte todo en minuscula
+    const nombrePokemon=document.getElementById("PonerPokemon").value.toLowerCase();
+
+    const respuesta=await fetch(`https://corsproxy.io/?https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`);
+    const data = await respuesta.json();
+
+    pokemonDiv.innerHTML=` <h2>${data.name}</h2>
+    <img src="${data.sprites.front_default}">
+    `
+    MostrarPokemon()
+});
