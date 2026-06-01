0d416e39522faa6fec37589d87a7bebcef4021a0
rrando-design <rrando@obralapiedad.com.ar>
2026-06-01 13:18:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,20 +4,17 @@ const pokemonDiv= document.getElementById("pokemon");
 async function mostrarPokemon(nombre){
     
 
-boton.addEventListener("click", async()=>{
-    //esto llama al PonerPokemon, obtiene lo que tiene la caja de texto, y el lower lo convierte todo en minuscula
-    const nombrePokemon=document.getElementById("PonerPokemon").value.toLowerCase();
    
     try{
         
     const respuesta=await fetch(`https://corsproxy.io/?https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`);
      if (!respuesta.ok) {
-     throw new Error("pokemon no encontrado "); } else
+     throw new Error("pokemon no encontrado "); } 
     const data = await respuesta.json();
-    console.log(name);
     console.log(data);
 
-    pokemonDiv.innerHTML=` <h2>${data.name}</h2>
+    pokemonDiv.innerHTML=
+        `<h2>${data.name}</h2>
     <img src="${data.sprites.front_default}">
     `
    
@@ -25,6 +22,9 @@ boton.addEventListener("click", async()=>{
         console.error(error.message);
     }
     
+boton.addEventListener("click", async()=>{
+    //esto llama al PonerPokemon, obtiene lo que tiene la caja de texto, y el lower lo convierte todo en minuscula
+    const nombrePokemon=document.getElementById("PonerPokemon").value.toLowerCase();
     mostrarPokemon()
 });
 }
