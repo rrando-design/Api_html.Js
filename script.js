5502b41475d4a8bdb2bfbff678ccc5d3f6c263df
rrando-design <rrando@obralapiedad.com.ar>
2026-06-01 13:40:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,20 @@
 const boton = document.getElementById("buscar");
 const pokemonDiv = document.getElementById("pokemon");
 
-
-boton.addEventListener("click", () => {
+boton.addEventListener("click", async () => {
     const nombrePokemon = document
         .getElementById("PonerPokemon")
         .value
         .toLowerCase();
 
-    mostrarPokemon(nombrePokemon);
+    const respuesta = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`
+    );
+
+    const data = await respuesta.json();
+
+    pokemonDiv.innerHTML = `
+        <h2>${data.name}</h2>
+        <img src="${data.sprites.front_default}">
+    `;
 });
-async function mostrarPokemon(nombrePokemon) {
-    try {
-        const respuesta = await fetch(
-            `https://pokeapi.co/api/v2/pokemon/${nombre}`
-        );
-
-        if (!respuesta.ok) {
-            throw new Error("Pokemon no encontrado");
-        }
-
-        const data = await respuesta.json();
-
-        pokemonDiv.innerHTML = `
-            <h2>${data.name}</h2>
-            <img src="${data.sprites.front_default}">
-        `;
-    } catch (error) {
-        console.error(error);
-    }
-}
-
